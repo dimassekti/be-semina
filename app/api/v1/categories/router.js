9484f8aabd7b722544748b68ccc9daeb6dcacf9b
Dimas Sekti Adji <dimassekti.adji@gmail.com>
2022-10-28 16:41:11 +0700

    Sesi 4
    Sesi 4

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express();
 
-router.get("/", (req, res) => {
-  res.send({ data: "categories" });
-});
+const { create, index, find, update, destroy } = require("./controller");
+
+router.get("/", index);
+router.post("/", create);
+router.get("/:id", find);
+router.put("/:id", update);
+router.delete("/:id", destroy);
+
+// router.get("/", (req, res) => {
+//   res.send({ data: "categories" });
+// });
 
 module.exports = router;
