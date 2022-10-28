24ab3c885727432161980512ef42f255717ff34f
Dimas Sekti Adji <dimassekti.adji@gmail.com>
2022-10-28 16:41:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const categoriesRouter = require("./app/api/v1/categories/router");
 const talentsRouter = require("./app/api/v1/talents/router");
 const urlV1 = "/api/v1/cms";
 
+const notFoundMiddleware = require("./app/middlewares/not-found");
+const handleErrorMiddleware = require("./app/middlewares/handler-error");
+
 const app = express();
 
 app.use(logger("dev"));
@@ -17,5 +20,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(`${urlV1}/categories`, categoriesRouter);
 app.use(`${urlV1}/talents`, talentsRouter);
+
+app.use(notFoundMiddleware);
+app.use(handleErrorMiddleware);
 
 module.exports = app;
